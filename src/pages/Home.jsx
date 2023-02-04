3e075db3fe124de59a5205d5d9cf36b8d9347dda
Shawn1303 <65193978+Shawn1303@users.noreply.github.com>
2023-02-03 21:35:31 -0500

    remove :D

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className=" flex flex-wrap justify-center items-center w-full h-screen">
     <div className="flex flex-col px-36 py-36 bg-white border h-2/3 rounded-lg">
-        <h1 className="text-black">Please sign up or login :D</h1>
+        <h1 className="text-black">Please sign up or login </h1>
      </div>
     </div>
   )
