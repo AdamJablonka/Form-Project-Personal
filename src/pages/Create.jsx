f0e0c4872867775b766625a25b87ea5cfb91045c
AdamJablonka <ajjablonka@gmail.com>
2023-02-04 14:56:19 -0500

    style changes to create page

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+// create.jsx needs to check if a user is auth'd before being able to create a form request to the databse.
 
 const Create = () => {
 
-  
+
   return (
     <div className="flex flex-wrap justify-center items-center w-full h-screen">
-      <div className='bg-white text-black rounded-md w-64 h-64 max-w-3xl border border-black flex justify-center drop-shadow-lg'>
+      <div className='bg-white text-black rounded-md w-64 h-64 max-w-3xl border border-custom-dark-brown flex justify-center drop-shadow-lg'>
         <form className='flex flex-col justify-center items-center font-medium'>
           <label for="stitle" className="m-2">Post Title</label>
           <input type="text" id="stitle" name="stitle" placeholder="Post Title" className='block p-1 border rounded-lg w-full border-custom-dark-brown focus:outline-none focus:ring-2'></input>
           
-          <textarea id="body" className="block p-2.5 w-full text-sm rounded-lg border border-custom-dark-brown resize-none mt-2 focus:outline-none focus:ring-2" placeholder="Survey body goes here..."></textarea>
+          <textarea id="body" className="block p-2.5 w-full text-sm rounded-lg border border-custom-dark-brown resize-none mt-2 focus:outline-none focus:ring-2" placeholder="Post body goes here..."></textarea>
 
-          <input type="submit" className='bg-custom-orange rounded-md m-3 w-2/3 cursor-pointer border border-custom-dark-brown focus:outline-none focus:ring-2' placeholder="Write your thoughts here..."></input>
+          <input type="submit" className='text-off-white bg-custom-blue rounded-md m-3 w-2/3 cursor-pointer focus:outline-none ring-2 focus:ring-4 p-1' placeholder="Write your thoughts here..."></input>
         </form>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,14 @@ module.exports = {
   ],
   theme: {
     colors:{
-      'custom-orange': '#e1b711',
+      'custom-light-orange': '#e1b711',
+      'custom-orange': '#ffa500',
       'custom-dark-brown': '#524d32',
       'custom-dark-gray': '#2c2e31',
       'white': '#dfe2d7',
       'black': '#000000',
       'custom-blue': '#0948d4',
+      'off-white': '#d9e0e8',
     },
     extend: {
       animation: {
