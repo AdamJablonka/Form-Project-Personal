c00b8f919a8e2df929353123a57a94164a3057fe
AdamJablonka <ajjablonka@gmail.com>
2023-02-04 19:14:25 -0500

    assignment create function working for title & body. Create page complete and assignment page half done

--- src/pages/Assignment.jsx
+++ src/pages/Assignment.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const Assignment = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPosts()
+  }, [])
+
+  function getPosts(){
+    const postRef = collection(db, 'posts')
+    getDocs(postRef).then(res => {
+      const postArray = res.docs.map(doc => ({
+        data: doc.data(),
+        id: doc.id,
+      }))
+      setPosts(postArray)
+      console.log("id:", posts)
+    })
+    .catch(error => console.log(error.message))
+  }
+
   return (
-    <div className="z-1 pt-16 flex flex-wrap justify-center items-center">
-      <div className="flex-row bg-white justify-center text-black rounded-md w-1/2 h-screen max-w-3xl border border-custom-dark-brown drop-shadow-lg">
-        <h1>Data will appear here:</h1>
-        <p>Test</p>
+    <div className="z-1 pt-16 flex justify-center items-center">
+      <div className="flex flex-col bg-white text-black rounded-md w-1/2 h-fit border border-custom-dark-brown drop-shadow-lg">
+        <h1 className='flex justify-center'>Data will appear here:</h1>
+        <button id="refresh_button" className="w-36 bg-custom-blue text-white p-1.5 m-1 rounded-md" onClick={() => getPosts()}>Refresh Posts</button>
+        <ul>
+          {posts.map(post => (
+            <div className='m-2 border border-custom-dark-brown rounded-md text-black flex-row'>
+              <p key={post.id} className='font-medium flex justify-center'>{post.data.title}</p>
+              <p key={post.id} className='font-medium flex justify-center'>{post.data.body}</p>
+            </div>
+          ))}
+        </ul>   
       </div>
     </div>
   )
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,19 +1,50 @@
-import React from 'react'
 // create.jsx needs to check if a user is auth'd before being able to create a form request to the databse.
+import React, { useState } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '../firebase'
 
 const Create = () => {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
 
+  function handleSubmit(e) {
+    e.preventDefault()
+    if (title === '' || body === '') {
+      alert("Title or Body can NOT be empty.")
+      return
+    }
+    const postsRef = collection(db, "posts"); 
+    addDoc(postsRef, {title, body})
+      .then(res => {
+        console.log("res: ", res.id)
+      })
+      .catch(error => {
+          console.log(error.message)
+      })
+    alert(title)
+  }
 
   return (
     <div className="flex flex-wrap justify-center items-center w-full h-screen">
       <div className='bg-white text-black rounded-md w-64 h-64 max-w-3xl border border-custom-dark-brown flex justify-center drop-shadow-lg'>
-        <form className='flex flex-col justify-center items-center'>
-          <label for="stitle" className="m-2 font-medium">Post Title</label>
-          <input type="text" id="stitle" name="stitle" placeholder="Post Title" className='block p-1 border rounded-lg w-full border-custom-dark-brown focus:outline-none focus:ring-2'></input>
-          
-          <textarea id="body" className="block p-2.5 w-full text-sm rounded-lg border border-custom-dark-brown resize-none mt-2 focus:outline-none focus:ring-2" placeholder="Post body goes here..."></textarea>
+        <form onSubmit={handleSubmit} className='flex flex-col justify-center items-center'>
+          <label htmlFor="title" className="m-2 font-medium">Post Title</label>
+          <input id="title" 
+          value={title} type="text" 
+          placeholder="Post Title" 
+          onChange={ e => setTitle(e.target.value)}
+          className='block p-1 border rounded-lg w-full border-custom-dark-brown focus:outline-none focus:ring-2'>  
+          </input>
 
-          <input type="submit" className='text-off-white bg-custom-blue rounded-md m-3 w-2/3 cursor-pointer focus:outline-none ring-2 focus:ring-4 p-1' placeholder="Write your thoughts here..."></input>
+          <label htmlFor="body" className="m-2 font-medium" onChange={ e => setBody(e.target.value)}> Post Body </label>
+          <textarea 
+          id="body" 
+          className="block p-2.5 w-full text-sm rounded-lg border border-custom-dark-brown resize-none focus:outline-none focus:ring-2" 
+          placeholder="Post body goes here..."
+          onChange={ e => setBody(e.target.value)}>
+          </textarea>
+
+          <input type="submit" className="text-off-white bg-custom-blue rounded-md m-3 w-2/3 cursor-pointer focus:outline-none ring-2 focus:ring-4 p-1" placeholder="Write your thoughts here..."></input>
         </form>
       </div>
     </div>
